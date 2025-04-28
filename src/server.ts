b9592e01bd92a5d4eb861171771dd3d1918729a8
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2025-04-28 18:53:46 +0200

    Predeployment setup

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import {
 } from "./lib/types";
 import { admin, db } from "./lib/firebaseAdmin";
 import { sendEmail } from "./lib/emailService";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
@@ -18,12 +19,31 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    status: 429,
+    error: "Too many requests, please try again after 15 minutes",
+  },
+});
+const alertLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 10,
+  message: {
+    status: 429,
+    error: "Too many alerts created; please wait an hour",
+  },
+});
+app.use(globalLimiter);
 export function verifyFirebaseToken(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  console.log("Running veryfication");
+  console.log("Running verification");
 
   const authHeader = req.headers.authorization;
 
@@ -37,9 +57,35 @@ export function verifyFirebaseToken(
   admin
     .auth()
     .verifyIdToken(token)
-    .then((decodedToken) => {
+    .then(async (decodedToken) => {
       (req as any).user = decodedToken;
       console.log("Authorized: Access granted");
+
+      try {
+        const { uid, email } = decodedToken;
+        const now = admin.firestore.FieldValue.serverTimestamp();
+        const route = req.route?.path || req.path;
+        const safeRoute = route.replace(/\//g, "_");
+
+        const userInRouteRef = db
+          .collection("stats_routes_users")
+          .doc(safeRoute)
+          .collection("users")
+          .doc(uid);
+
+        await userInRouteRef.set(
+          {
+            userId: uid,
+            email: email || null,
+            route: route,
+            count: admin.firestore.FieldValue.increment(1),
+            lastRequest: now,
+          },
+          { merge: true }
+        );
+      } catch (error) {
+        console.error("⚠️ Failed to update stats:", error);
+      }
 
       next();
     })
@@ -53,96 +99,107 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Server is running 🚀" });
 });
 
-app.get("/stocks", verifyFirebaseToken, async (req: Request, res: Response) => {
-  try {
-    const symbols: string[] = ["AAPL", "MSFT", "GOOGL", "AMZN"];
-    const results: {
-      name: string;
-      symbol: string;
-      currentPrice: number;
-      percentChange: string;
-      high: number;
-      low: number;
-    }[] = [];
+app.get(
+  "/stocks",
+  verifyFirebaseToken,
 
-    for (const symbol of symbols) {
-      const response = await axios.get<FinnhubQuoteResponse>(
-        `https://finnhub.io/api/v1/quote`,
-        {
-          params: {
-            symbol,
-            token: process.env.FINNHUB_API_KEY,
-          },
-        }
-      );
+  async (req: Request, res: Response) => {
+    try {
+      const symbols: string[] = ["AAPL", "MSFT", "GOOGL", "AMZN"];
+      const results: {
+        name: string;
+        symbol: string;
+        currentPrice: number;
+        percentChange: string;
+        high: number;
+        low: number;
+      }[] = [];
 
-      results.push({
-        name: symbol,
-        symbol: symbol,
-        currentPrice: response.data.c,
-        percentChange: (
-          ((response.data.c - response.data.pc) / response.data.pc) *
-          100
-        ).toFixed(2),
-        high: response.data.h,
-        low: response.data.l,
-      });
+      for (const symbol of symbols) {
+        const response = await axios.get<FinnhubQuoteResponse>(
+          `https://finnhub.io/api/v1/quote`,
+          {
+            params: {
+              symbol,
+              token: process.env.FINNHUB_API_KEY,
+            },
+          }
+        );
+
+        results.push({
+          name: symbol,
+          symbol: symbol,
+          currentPrice: response.data.c,
+          percentChange: (
+            ((response.data.c - response.data.pc) / response.data.pc) *
+            100
+          ).toFixed(2),
+          high: response.data.h,
+          low: response.data.l,
+        });
+      }
+
+      res.json(results);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to fetch stock data" });
+    }
+  }
+);
+
+app.get(
+  "/stock",
+  verifyFirebaseToken,
+
+  async (req: Request, res: Response) => {
+    const { symbol } = req.query;
+
+    if (!symbol || typeof symbol !== "string") {
+      res.status(400).json({ message: "Symbol is required" });
+      return;
     }
 
-    res.json(results);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Failed to fetch stock data" });
-  }
-});
-
-app.get("/stock", verifyFirebaseToken, async (req: Request, res: Response) => {
-  const { symbol } = req.query;
-
-  if (!symbol || typeof symbol !== "string") {
-    res.status(400).json({ message: "Symbol is required" });
-    return;
-  }
-
-  try {
-    const [quoteRes, profileRes] = await Promise.all([
-      axios.get<FinnhubQuoteResponse>(`https://finnhub.io/api/v1/quote`, {
-        params: {
-          symbol,
-          token: process.env.FINNHUB_API_KEY,
-        },
-      }),
-      axios.get<FinnhubProfileResponse>(
-        `https://finnhub.io/api/v1/stock/profile2`,
-        {
+    try {
+      const [quoteRes, profileRes] = await Promise.all([
+        axios.get<FinnhubQuoteResponse>(`https://finnhub.io/api/v1/quote`, {
           params: {
             symbol,
             token: process.env.FINNHUB_API_KEY,
           },
-        }
-      ),
-    ]);
+        }),
+        axios.get<FinnhubProfileResponse>(
+          `https://finnhub.io/api/v1/stock/profile2`,
+          {
+            params: {
+              symbol,
+              token: process.env.FINNHUB_API_KEY,
+            },
+          }
+        ),
+      ]);
 
-    res.json({
-      symbol,
-      name: profileRes.data.name, //
-      currentPrice: quoteRes.data.c,
-      percentChange: (
-        ((quoteRes.data.c - quoteRes.data.pc) / quoteRes.data.pc) *
-        100
-      ).toFixed(2),
-      high: quoteRes.data.h,
-      low: quoteRes.data.l,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Failed to fetch stock data" });
+      res.json({
+        symbol,
+        name: profileRes.data.name,
+        currentPrice: quoteRes.data.c,
+        percentChange: (
+          ((quoteRes.data.c - quoteRes.data.pc) / quoteRes.data.pc) *
+          100
+        ).toFixed(2),
+        high: quoteRes.data.h,
+        low: quoteRes.data.l,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to fetch stock data" });
+    }
   }
-});
+);
 
 app.post(
   "/postalert",
   verifyFirebaseToken,
+  alertLimiter,
   async (req: Request, res: Response) => {
     const { symbol, targetPrice, alertName, direction } = req.body;
     const uid = (req as any).user.uid;
@@ -177,28 +234,37 @@ app.post(
   }
 );
 
-app.get("/alerts", verifyFirebaseToken, async (req: Request, res: Response) => {
-  const uid = (req as any).user.uid;
+app.get(
+  "/alerts",
+  verifyFirebaseToken,
 
-  try {
-    const q = admin.firestore().collection("alerts").where("userId", "==", uid);
-    const snapshot = await q.get();
+  async (req: Request, res: Response) => {
+    const uid = (req as any).user.uid;
 
-    const alerts = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    try {
+      const q = admin
+        .firestore()
+        .collection("alerts")
+        .where("userId", "==", uid);
+      const snapshot = await q.get();
 
-    res.status(200).json(alerts);
-  } catch (error) {
-    console.error("Error fetching alerts:", error);
-    res.status(500).json({ message: "Failed to fetch alerts" });
+      const alerts = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      res.status(200).json(alerts);
+    } catch (error) {
+      console.error("Error fetching alerts:", error);
+      res.status(500).json({ message: "Failed to fetch alerts" });
+    }
   }
-});
+);
 
 app.delete(
   "/alerts/:id",
   verifyFirebaseToken,
+
   async (req: Request, res: Response) => {
     const uid = (req as any).user.uid;
     const { id } = req.params;
@@ -228,6 +294,25 @@ app.delete(
   }
 );
 
+app.get("/stats", verifyFirebaseToken, async (_req: Request, res: Response) => {
+  try {
+    const statsSnapshot = await admin
+      .firestore()
+      .collectionGroup("users")
+      .get();
+
+    const stats = statsSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    res.status(200).json(stats);
+  } catch (error) {
+    console.error("Error fetching all stats:", error);
+    res.status(500).json({ message: "Failed to fetch stats" });
+  }
+});
+
 cron.schedule("*/5 * * * *", async () => {
   console.log("🔍 Running alert checker...");
 
@@ -251,9 +336,8 @@ cron.schedule("*/5 * * * *", async () => {
     try {
       const { id, symbol, userId, targetPrice, direction } = alert;
 
-      // 1) fetch current price
       const { data } = await axios.get<{
-        c: number; // current price
+        c: number;
       }>("https://finnhub.io/api/v1/quote", {
         params: {
           symbol,
@@ -262,7 +346,6 @@ cron.schedule("*/5 * * * *", async () => {
       });
       const currentPrice = data.c;
 
-      // 2) determine if it's time to fire
       let shouldTrigger = false;
       if (direction === "above" && currentPrice >= targetPrice) {
         shouldTrigger = true;
@@ -281,7 +364,6 @@ cron.schedule("*/5 * * * *", async () => {
 
       console.log(`🚨 Alert triggered for ${symbol} at ${currentPrice}`);
 
-      // 3) lookup user & send email
       const userRecord = await admin.auth().getUser(userId);
       const email = userRecord.email;
       if (email) {
@@ -295,7 +377,6 @@ cron.schedule("*/5 * * * *", async () => {
         console.log(`📩 Email sent to ${email}`);
       }
 
-      // 4) mark as triggered
       await admin.firestore().collection("alerts").doc(id).update({
         triggered: true,
         triggeredAt: admin.firestore.FieldValue.serverTimestamp(),
